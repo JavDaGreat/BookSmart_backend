357807fb0186cfdeadf3f86968d6bac76f709768
Javad <1998javad@gmail.com>
2024-04-07 20:02:27 +0200

    Update server.js to connect to MongoDB and change the port

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
+const connectDB = require("./config/ConnectDB");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3500;
+connectDB();
+
+mongoose.connection.once("open", () => {
+  console.log("connected to mango");
+
+  app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
+});
