93d3025567f11c8d41bf522ea91aaeeb56e4aa26
JavDaGreat <98164907+JavDaGreat@users.noreply.github.com>
2024-04-17 14:32:50 +0200

    add admin roll in api

--- controller/AppointmentController.js
+++ controller/AppointmentController.js
@@ -1,15 +1,31 @@
 const Appointment = require("../models/Appointment");
+const Company = require("../models/Company");
 
 const getAllAppointments = async (req, res) => {
-  const { id } = req.body;
+  const { id, isAdmin, companyId } = req.body;
   if (!id) {
     return res.status(404).json({ message: "User not Found" });
   }
-  const appointments = await Appointment.find({ authorizedUsers: id }).exec();
-  if (!appointments || appointments.length === 0) {
-    return res.status(204).send();
+
+  let appointments;
+  try {
+    if (isAdmin) {
+      const company = await Company.findById(companyId).populate(
+        "appointments"
+      );
+      appointments = company.appointments;
+    } else {
+      appointments = await Appointment.find({ authorizedUsers: id }).exec();
+    }
+
+    if (!appointments || appointments.length === 0) {
+      return res.status(204).send();
+    }
+
+    res.json(appointments);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
-  res.json(appointments);
 };
 const createAppointment = async (req, res) => {
   const { companyId, date, time, description, createdBy, authorizedUsers } =
@@ -35,4 +51,72 @@ const createAppointment = async (req, res) => {
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
+};
+const updateAppointment = async (req, res) => {
+  const { createdBy, updatedAppointment, isAdmin, appointmentId } = req.body;
+  if (!createdBy || !updatedAppointment) {
+    return res
+      .status(400)
+      .json({ message: "Please provide all the required fields" });
+  }
+
+  try {
+    let appointment;
+    if (isAdmin) {
+      appointment = await Appointment.findByIdAndUpdate(
+        appointmentId,
+        { $set: updatedAppointment },
+        { new: true }
+      );
+    } else {
+      appointment = await Appointment.findOneAndUpdate(
+        { _id: appointmentId, createdBy },
+        { $set: updatedAppointment },
+        { new: true }
+      );
+    }
+
+    if (!appointment) {
+      return res.status(404).json({ message: "Appointment not found" });
+    }
+
+    res.json(appointment);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+const deleteAppointment = async (req, res) => {
+  const { createdBy, appointmentId, isAdmin } = req.body;
+  if (!createdBy || !appointmentId) {
+    return res
+      .status(400)
+      .json({ message: "Please provide all the required fields" });
+  }
+
+  try {
+    let deletedAppointment;
+    if (isAdmin) {
+      deletedAppointment = await Appointment.deleteOne({ _id: appointmentId });
+    } else {
+      deletedAppointment = await Appointment.deleteOne({
+        _id: appointmentId,
+        createdBy,
+      });
+    }
+
+    if (deletedAppointment.deletedCount === 0) {
+      return res.status(404).json({ message: "Appointment not found" });
+    }
+
+    res.json({ message: "Appointment deleted successfully" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+module.exports = {
+  getAllAppointments,
+  createAppointment,
+  updateAppointment,
+  deleteAppointment,
 };
