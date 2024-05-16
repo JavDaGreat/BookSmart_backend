7bb62e62d37aa8d455227fc2bf560be6ba618678
Js-PC <1998javad@gmail.com>
2024-05-16 16:21:30 +0200

    chore: Add 'title' field to Appointment model

--- model/Appointment.js
+++ model/Appointment.js
@@ -2,6 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const AppointmentSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
   companyId: {
     type: String,
     ref: "Business",
